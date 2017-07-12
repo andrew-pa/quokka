bd53ea87f532c5c0f9a661e812cbc5dbec74ec33
Andrew Palmer <andrew.pa@outlook.com>
2017-07-12 13:30:04 -0700

    server: add redis connection

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,16 +3,25 @@ extern crate tokio_core;
 extern crate tokio_io;
 extern crate tokio_proto;
 extern crate tokio_service;
-
+extern crate r2d2;
+extern crate r2d2_redis;
+extern crate redis;
 extern crate protocol;
 
+use std::default::Default;
 use tokio_proto::TcpServer;
+use r2d2_redis::RedisConnectionManager;
+
+use std::io::{Error as IOError, ErrorKind as IOErrorKind};
 
 mod service;
 
-
-
 fn main() {
     let addr = std::env::var("QUOKKA_ADDR").or(Ok(String::from("127.0.0.1:3333"))).and_then(|s| s.parse()).expect("server address");
-    TcpServer::new(protocol::IMProto, addr).serve(|| Ok(service::IMService));
+    let dbm = RedisConnectionManager::new(match std::env::var("QUOKKA_REDIS_URL") {
+        Ok(ar) => ar,
+        Err(_) => String::from("redis://localhost")
+    }.as_ref()).expect("redis db");
+    let dbpool = r2d2::Pool::new(Default::default(), dbm).expect("db connection pool");
+    TcpServer::new(protocol::IMProto, addr).serve(move || Ok(service::IMService { dbc: dbpool.get().map_err(|_| IOError::new(IOErrorKind::TimedOut, ""))? }));
 }
--- server/src/service.rs
+++ server/src/service.rs
@@ -1,11 +1,14 @@
-
 use std::io::Error as IOError;
 use futures::{future, Future, BoxFuture};
 use tokio_service::Service;
-
 use protocol::{Request, Response};
+use r2d2;
+use r2d2_redis::RedisConnectionManager;
+use redis::{Client, Connection, Commands};
 
-pub struct IMService;
+pub struct IMService {
+    pub dbc: r2d2::PooledConnection<RedisConnectionManager>
+}
 
 impl Service for IMService {
     type Request = Request;
@@ -16,6 +19,8 @@ impl Service for IMService {
     type Future = BoxFuture<Self::Response, Self::Error>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
+        let n: i64 = self.dbc.incr("request_counter", 1).expect("incr req counter");
+        println!("{:?}, {}", req, n);
         future::ok(Response::ACK).boxed()
     }
 }
