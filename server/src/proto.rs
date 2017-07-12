729da754ecf184a3d98218a2b5abc023dbc92b0d
Andrew Palmer <andrew.pa@outlook.com>
2017-07-12 10:41:38 -0700

    server: stub in more infra

--- server/src/proto.rs
+++ server/src/proto.rs
@@ -1,14 +1,28 @@
 
-use std::io::Error as IOError;
-use tokio_io::{AsyncRead, AsyncWrite};
+use std::error::Error;
 use bytes::{BufMut, BytesMut};
+use tokio_io::{AsyncRead, AsyncWrite};
 use tokio_io::codec::{Framed, Decoder, Encoder};
 use tokio_proto::pipeline::ServerProto;
+use std::io::Error as IOError;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Request {
+    Init {
+        name: String
+    },
+    Ping
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Response {
+    ACK
+}
 
 pub struct IMCodec;
 
 impl Decoder for IMCodec {
-    type Item = u8;
+    type Item = Request;
     type Error = IOError;
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         Ok(None)
@@ -16,18 +30,18 @@ impl Decoder for IMCodec {
 }
 
 impl Encoder for IMCodec {
-    type Item = u8;
+    type Item = Response;
     type Error = IOError;
     fn encode(&mut self, item: Self::Item, dest: &mut BytesMut) -> Result<(), Self::Error> {
-        Ok(dest.put(item))
+        Ok(())
     }
 }
 
 pub struct IMProto;
 
 impl<T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for IMProto {
-    type Request = u8;
-    type Response = u8;
+    type Request = Request;
+    type Response = Response;
     type Transport = Framed<T, IMCodec>;
     type BindTransport = Result<Self::Transport, IOError>;
 
--- server/src/service.rs
+++ server/src/service.rs
@@ -3,17 +3,19 @@ use std::io::Error as IOError;
 use futures::{future, Future, BoxFuture};
 use tokio_service::Service;
 
+use proto::{Request, Response};
+
 pub struct IMService;
 
 impl Service for IMService {
-    type Request = u8;
-    type Response = u8;
+    type Request = Request;
+    type Response = Response;
 
     type Error = IOError;
 
     type Future = BoxFuture<Self::Response, Self::Error>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
-        future::ok(req).boxed()
+        future::ok(Response::ACK).boxed()
     }
 }
